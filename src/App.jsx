cd0d4fea6684b8d740f6bec88c1e561dcff728e9
noah berlove <noah.berlove@gmail.com>
2022-10-19 12:15:06 -0700

    add basic functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userNames, setUserNames] = useState([])
+  const [currentName, setCurrentName] = useState("")
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    setUserNames(prevNames => {
+      return [
+              ...prevNames, 
+              currentName
+            ]
+          })
+    setCurrentName("")      
+  }
+  console.log(currentName)
 
+  function handleChange(event) {
+    event.preventDefault() 
+    setCurrentName(event.target.value)
+  }
+  
+  let namesHTML = userNames.map(name => {
+    return (
+      <div>
+        <h1>{name}</h1>
+      </div>
+    )
+  }
+  )
+
+  console.log(namesHTML)
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{currentName}</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+            type="text" 
+            name="newName" 
+            onChange={handleChange} 
+            value={currentName}>
+        </input>
+        <button>Submit</button>
+      </form>
+      {namesHTML}
     </div>
   )
 }
